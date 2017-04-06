97ccd8ce226ba51ccf263ab3a6a7fd5d0afafb12
Prysiazhniuk Anastasiia <anastasiia.prysiazhniuk@etu.umontpellier.fr>
2017-04-06 12:44:29 +0200

    Creation de fichier out.arff avec des lemmes dans le DatasrtLemmes

--- Dataset/transformationLigne.py
+++ Dataset/transformationLigne.py
@@ -1,23 +1,18 @@
-
-
-source = open("dataResult2.txt","r")
+source = open("dataset3.txt","r")
 destination = open("lemmesLignes.txt", "wt")
 output = ""
-
-
-a = "hello"
-b = "world"
-a += b
-print a
 
 for ligne in source:
 	mot = ligne.split("\t")[1]
 	token = mot.split("\n")[0]
 	token2 = token.split("\r")[0]
-	output = output + " "+ token2
-	
-	if ligne.split("\t")[0]=="SENT":
+	if token2=="ENDOFLINE":
 		destination.write(output+"\n")
 		output = ""
+                token2 = ""
+        else:        
+       	        output = output + " "+ token2
+	
+        
 source.close()
 destination.close()
